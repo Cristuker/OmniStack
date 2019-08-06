9c116c5dc07b051e8e07315fd4698a2d7f25f794
Cristuker <cristian123105@gmail.com>
2019-08-06 16:18:07 -0300

    add rotas de like e dislike

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,12 @@
 const express = require('express')
-
+const DevController = require('./controllers/DevController')
+const LikeController = require('./controllers/LikeController')
+const DislikeController = require('./controllers/DislikeController')
 const routes = express.Router();
 
-routes.get('/', (req,res) => {
-     //recebendo parametros pela URL
-    return res.send(`Salve ${req.query.name}`)
-});
-
-routes.post('/devs', (req,res) =>{
-    return res.json(req.body);
-})
+routes.get('/devs', DevController.index)
+routes.post('/devs',DevController.store)
+routes.post('/devs/:devId/likes', LikeController.store)
+routes.post('/devs/:devId/dislikes', DislikeController.store)
 
 module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const routes = require('./routes')
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors')
 const server = express();
+
+mongoose.connect('mongodb+srv://cristuker:136crcc12@cluster0-gszt9.azure.mongodb.net/tindev?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+});
+
+server.use(cors());
 
 server.use(express.json())
 server.use(routes)
@@ -8,5 +16,5 @@ server.use(routes)
 server.listen(3333);
 //escutando a porta 3333 ou sejá o servidor está rodando aqui
 /*
--D para instalar dependencia de desenvolvimento
+-D para instalar dependencia de desenvolvimento  
 */
