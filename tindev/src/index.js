85d372d4d3865c720bd51066c6ca2fe04c35fa8b
Cristuker <cristian123105@gmail.com>
2019-08-09 15:25:03 -0300

    finalizando mobile

---
+++ tindev/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Routes from './routes'
+
+
+function App (){
+  return (
+   <Routes />
+  );
+};
+
+
+
+
+export default App;
+/*
+View no lugar da div
+text = p , span, strong...
+as tags n tem estilização propria igual no html é tudo text quando for inserir um texto
+
+
+
+*/
